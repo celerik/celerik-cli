8787e7d3372f3a67fa096eef2769a37a6f678b74
Miguel Vélez <mvelezg99@gmail.com>
2021-01-21 15:51:55 -0500

    Implement create folder and clone boilerplate repo
    - Add json with the boilerplates repos
    - Implement logic to create folder and clone the boilerplate repo depending on the selected option

--- src/create-cli.ts
+++ src/create-cli.ts
@@ -1,6 +1,24 @@
+// @packages
 import yargs from 'yargs';
+import fs from 'fs';
+import { exec } from 'child_process';
+
+// @scripts
+import boilerplates from './boilerplates/celerik-boilerplates.json';
+
+// @types
+type ObjectType = {
+    rootPath: string;
+    starter: string;
+};
+
+type BoilerplateType = {
+    [key: string]: string;
+};
 
 export function createCli(argv: Array<string>): yargs.Arguments {
+    const boilerplatesObject = <BoilerplateType>boilerplates;
+
     const cli = yargs(argv).parserConfiguration({
         'boolean-negation': false,
     });
@@ -20,9 +38,28 @@ export function createCli(argv: Array<string>): yargs.Arguments {
         .command({
             command: 'create [rootPath] [starter]',
             describe: 'Create a new project with @celerik/cli',
-            handler: ({ rootPath, starter }) => {
-                console.log(rootPath);
-                console.log(starter);
+            handler: ({ rootPath, starter }: ObjectType) => {
+                try {
+                    console.log(rootPath);
+                    console.log(starter);
+                    console.log(boilerplatesObject[starter]);
+                    const boilerplateRepo = boilerplatesObject[starter];
+                    if (!fs.existsSync(rootPath)) {
+                        fs.mkdirSync(rootPath);
+                        exec(`cd ${rootPath} && git clone ${boilerplateRepo}`, (error, stdout, stderr) => {
+                            if (error) {
+                                console.log(`exec error: ${error}`);
+                                return;
+                            }
+                            console.log(`stdout: ${stdout}`);
+                            console.error(`stderr: ${stderr}`);
+                        });
+                    } else {
+                        console.log('This directory has already been created.');
+                    }
+                } catch (error) {
+                    console.log(`${error}: An error has occurred`);
+                }
             },
         })
         .demandCommand(1, 'Pass --help to see all available commands')
